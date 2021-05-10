c15fa6a8bee55dd3e393aa805b39c499d1014067
Alex Bucknall <alex.bucknall@gmail.com>
2021-05-10 20:56:23 +0000

    Adds modems to os test suite
    Change-type: minor
    Signed-off-by: Alex Bucknall <alexbucknall@balena.io>

--- tests/suites/os/suite.js
+++ tests/suites/os/suite.js
@@ -17,19 +17,20 @@ const stream = require('stream')
 const pipeline = require('bluebird').promisify(stream.pipeline);
 
 module.exports = {
-	title: 'Unmanaged BalenaOS release suite',
-	run: async function(test) {
+	title: "Unmanaged BalenaOS release suite",
+	run: async function () {
 		// The worker class contains methods to interact with the DUT, such as flashing, or executing a command on the device
-		const Worker = this.require('common/worker');
+		const Worker = this.require("common/worker");
+
 		// The balenaOS class contains information on the OS image to be flashed, and methods to configure it
-		const BalenaOS = this.require('components/os/balenaos');
+		const BalenaOS = this.require("components/os/balenaos");
 
 		await fse.ensureDir(this.suite.options.tmpdir);
 
 		// The suite contex is an object that is shared across all tests. Setting something into the context makes it accessible by every test
 		this.suite.context.set({
-			utils: this.require('common/utils'),
-			sshKeyPath: join(homedir(), 'id'),
+			utils: this.require("common/utils"),
+			sshKeyPath: join(homedir(), "id"),
 			link: `${this.suite.options.balenaOS.config.uuid.slice(0, 7)}.local`,
 			worker: new Worker(this.suite.deviceType.slug, this.getLogger()),
 		});
@@ -67,26 +68,24 @@ module.exports = {
 									.utils.createSSHKey(this.context.get().sshKeyPath),
 							],
 						},
-						// Set an API endpoint for the HTTPS time sync service.
-						apiEndpoint: 'https://api.balena-cloud.com',
 						// persistentLogging is managed by the supervisor and only read at first boot
 						persistentLogging: true,
 						// Set local mode so we can perform local pushes of containers to the DUT
 						localMode: true,
-						developmentMode: true,
 					},
 				},
-				this.getLogger(),
+				this.getLogger()
 			),
 		});
 
 		// Register a teardown function execute at the end of the test, regardless of a pass or fail
 		this.suite.teardown.register(() => {
-			this.log('Worker teardown');
+			this.log("Removing image");
+			fse.unlinkSync("/data/image"); // Delete the unpacked an modified image from the testbot cache to prevent use in the next suite
+			this.log("Worker teardown");
 			return this.context.get().worker.teardown();
 		});
-
-		this.log('Setting up worker');
+		this.log("Setting up worker");
 
 		// Create network AP on testbot
 		await this.context
@@ -94,33 +93,11 @@ module.exports = {
 			.worker.network(this.suite.options.balenaOS.network);
 
 		// Unpack OS image .gz
-		await this.context.get().os.fetch();
-
-		// If this is a flasher image, and we are using qemu, unwrap
-		if(this.suite.deviceType.data.storage.internal && (process.env.WORKER_TYPE === `qemu`)){
-			const RAW_IMAGE_PATH = `/opt/balena-image-${this.suite.deviceType.slug}.balenaos-img`
-			const OUTPUT_IMG_PATH = '/data/downloads/unwrapped.img'
-			console.log(`Unwrapping file ${this.context.get().os.image.path}`)
-			console.log(`Looking for ${RAW_IMAGE_PATH}`)
-			try{
-				await imagefs.interact(this.context.get().os.image.path, 2, async (fsImg) => {
-					await pipeline(
-					fsImg.createReadStream(RAW_IMAGE_PATH),
-					fse.createWriteStream(OUTPUT_IMG_PATH)
-					)
-				})
-
-				this.context.get().os.image.path = OUTPUT_IMG_PATH;
-				console.log(`Unwrapped flasher image!`);
-			}catch(e){
-				// If the outer image doesn't contain an image for installation, ignore the error
-				if (e.code == 'ENOENT') {
-					console.log("Not a flasher image, skipping unwrap");
-				} else {
-					throw e;
-				}
-			}
-		}
+		await this.context.get().os.fetch({
+			type: this.suite.options.balenaOS.download.type,
+			version: this.suite.options.balenaOS.download.version,
+			releaseInfo: this.suite.options.balenaOS.releaseInfo,
+		});
 
 		// Configure OS image
 		await this.context.get().os.configure();
@@ -130,43 +107,33 @@ module.exports = {
 		await this.context.get().worker.flash(this.context.get().os.image.path);
 		await this.context.get().worker.on();
 
-		this.log('Waiting for device to be reachable');
+		this.log("Waiting for device to be reachable");
 		assert.equal(
 			await this.context
 				.get()
 				.worker.executeCommandInHostOS(
-					'cat /etc/hostname',
-					this.context.get().link,
+					"cat /etc/hostname",
+					this.context.get().link
 				),
-			this.context.get().link.split('.')[0],
-			'Device should be reachable',
+			this.context.get().link.split(".")[0],
+			"Device should be reachable"
 		);
-
-    // Retrieving journalctl logs: register teardown after device is reachable
-    this.suite.teardown.register(async () => {
-			await this.context.get().worker.archiveLogs(this.id, this.context.get().link);
-		});
-
 	},
 	tests: [
-		'./tests/device-specific-tests/beaglebone-black',
-		'./tests/fingerprint',
-		'./tests/fsck',
-		'./tests/os-release',
-		'./tests/issue',
-		'./tests/chrony',
-		'./tests/kernel-overlap',
-		'./tests/bluetooth',
-		'./tests/healthcheck',
-		'./tests/variables',
-		'./tests/led',
-		'./tests/config-json',
-		'./tests/boot-splash',
-		'./tests/connectivity',
-		'./tests/engine-socket',
-		'./tests/udev',
-		'./tests/device-tree',
-		'./tests/purge-data',
-		'./tests/device-specific-tests/revpi-core-3',
+		"./tests/fingerprint",
+		"./tests/os-release",
+		"./tests/chrony",
+		"./tests/kernel-overlap",
+		"./tests/bluetooth",
+		"./tests/healthcheck",
+		"./tests/variables",
+		"./tests/led",
+		"./tests/config-json",
+		"./tests/boot-splash",
+		"./tests/connectivity",
+		"./tests/engine-healthcheck",
+		"./tests/udev",
+		"./tests/device-tree",
+		"./tests/modem",
 	],
 };
